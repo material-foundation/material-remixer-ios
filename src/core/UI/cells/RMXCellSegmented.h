4bb74a1b8d9375f620fca7e5f39729f1e143f919
Andres Ugarte <chuga@google.com>
2016-10-11 12:16:11 -0400

    Nit

--- src/core/UI/cells/RMXCellSegmented.h
+++ src/core/UI/cells/RMXCellSegmented.h
@@ -16,14 +16,10 @@
 
 #import "RMXCell.h"
 
-#import "RMXVariable.h"
-
 NS_ASSUME_NONNULL_BEGIN
 
 /** Provides a table view cell that has a segmented control. */
 @interface RMXCellSegmented : RMXCell
-
-@property(nonatomic, weak) RMXVariable *variable;
 
 @end
 
