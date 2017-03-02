3490a357b7d34bacdcd8e8e75946c1e075176f27
Andres Ugarte <chuga@google.com>
2017-03-02 16:26:21 -0500

    First part of the new demo app

---
+++ examples/objc/RemixerExample/cells/SectionHeaderView.m
@@ -0,0 +1,46 @@
+/*
+ Copyright 2016-present Google Inc. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "SectionHeaderView.h"
+
+@interface SectionHeaderView ()
+
+@property(nonatomic, strong) UILabel *titleLabel;
+
+@end
+
+@implementation SectionHeaderView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+  self = [super initWithFrame:frame];
+  if (self) {
+    self.backgroundColor = [UIColor colorWithWhite:0.95 alpha:1];
+    _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    [_titleLabel setFont:[UIFont systemFontOfSize:16.0 weight:UIFontWeightUltraLight]];
+    [_titleLabel setTextColor:[UIColor colorWithWhite:0.5 alpha:1]];
+    [_titleLabel setText:@"Recent Transactions"];
+    [self addSubview:_titleLabel];
+  }
+  return self;
+}
+
+- (void)layoutSubviews {
+  [_titleLabel sizeToFit];
+  _titleLabel.frame = CGRectMake(16, self.bounds.size.height / 2.0 - _titleLabel.bounds.size.height / 2.0,
+                                 _titleLabel.bounds.size.width, _titleLabel.bounds.size.height);
+}
+
+@end
